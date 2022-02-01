1eeddeccc720fa1bbe4207ad888d838d4471a7e0
dev <dev@devs-MacBook-Pro.local>
2022-02-01 01:34:17 -0600

    Add modifiers, validations

--- contracts/FaucetContract.sol
+++ contracts/FaucetContract.sol
@@ -2,7 +2,58 @@
 pragma solidity >=0.4.22 <0.9.0;
 
 contract Faucet {
-    // storage variables
-    uint public funds = 1000;
-    int public counter = -10;
+    address public owner;
+    uint public numOfFunders;
+    mapping(address => bool) private funders;
+    mapping(uint => address) private lutFunders;
+
+    constructor() {
+        owner = msg.sender;
+    }
+
+    modifier limitWithdraw(uint withdrawAmount) {
+        require(
+            withdrawAmount < 1000000000000000000,
+            "Cannot withdraw more than 0.1 ether"
+        );
+        _;
+    }
+    modifier onlyOwner {
+        require(
+            msg.sender == owner,
+            "Only owner can call this function"
+        );
+        _;
+    }
+ 
+    receive() external payable {}
+
+    function addFunds() external payable {
+        address funder = msg.sender;
+
+        if (!funders[funder]) {
+            uint index = numOfFunders++;
+            funders[funder] = true;
+            lutFunders[index] = funder;
+        }
+    }
+
+    function withdraw(uint withdrawAmount) external limitWithdraw(withdrawAmount) {
+        payable(msg.sender).transfer(withdrawAmount);
+
+    }
+
+    function getAllFunders() external view returns (address[] memory) {
+        address[] memory _funders = new address[](numOfFunders);
+
+        for (uint i = 0; i < numOfFunders; i++) {
+            _funders[i] = lutFunders[i];
+        }
+
+        return _funders;
+    }
+
+    function getFunderAtIndex(uint8 index) external view returns(address) {
+        return lutFunders[index];
+    }
 }
